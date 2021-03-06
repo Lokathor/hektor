61206ebb84442a754aaa1c30ad4fdc8e68dd0545
Lokathor <zefria@gmail.com>
2021-03-06 01:56:00 -0700

    outline for transpositon.

---
+++ src/transpose.rs
@@ -0,0 +1,55 @@
+use super::*;
+
+impl Mat2x2 {
+  fn transpose(self) -> Mat2x2 {
+    todo!()
+  }
+}
+
+impl Mat2x3 {
+  fn transpose(self) -> Mat3x2 {
+    todo!()
+  }
+}
+
+impl Mat2x4 {
+  fn transpose(self) -> Mat4x2 {
+    todo!()
+  }
+}
+
+impl Mat3x2 {
+  fn transpose(self) -> Mat2x3 {
+    todo!()
+  }
+}
+
+impl Mat3x3 {
+  fn transpose(self) -> Mat3x3 {
+    todo!()
+  }
+}
+
+impl Mat3x4 {
+  fn transpose(self) -> Mat4x3 {
+    todo!()
+  }
+}
+
+impl Mat4x2 {
+  fn transpose(self) -> Mat2x4 {
+    todo!()
+  }
+}
+
+impl Mat4x3 {
+  fn transpose(self) -> Mat3x4 {
+    todo!()
+  }
+}
+
+impl Mat4x4 {
+  fn transpose(self) -> Mat4x4 {
+    todo!()
+  }
+}
