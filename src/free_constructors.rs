b7fe566e84279a9b6832269ec5c56477ccd48db6
Lokathor <zefria@gmail.com>
2021-03-06 01:52:00 -0700

    fill in all free constructors.

--- src/free_constructors.rs
+++ src/free_constructors.rs
@@ -12,14 +12,74 @@ pub const fn vec4(x: f32, y: f32, z: f32, w: f32) -> Vec4 {
   Vec4 { x, y, z, w }
 }
 
-pub const fn mat2x2_row(r0c0: f32, r0c1: f32, r1c0: f32, r1c1: f32) -> Mat2x2 {
-  Mat2x2 { r0c0, r0c1, r1c0, r1c1 }
+pub const fn mat2x2_rows(r0c0: f32, r1c0: f32, r0c1: f32, r1c1: f32) -> Mat2x2 {
+  Mat2x2 { r0c0, r1c0, r0c1, r1c1 }
 }
 
-pub const fn mat2x3_row(r0c0: f32, r0c1: f32, r0c2: f32, r1c0: f32, r1c1: f32, r1c2: f32) -> Mat2x3 {
-  Mat2x3 { r0c0, r0c1, r0c2, r1c0, r1c1, r1c2 }
+pub const fn mat2x3_rows(
+  r0c0: f32, r1c0: f32, r0c1: f32, r1c1: f32, r0c2: f32, r1c2: f32,
+) -> Mat2x3 {
+  Mat2x3 { r0c0, r1c0, r0c1, r1c1, r0c2, r1c2 }
 }
 
-pub const fn mat2x4_row(r0c0: f32, r0c1: f32, r0c2: f32, r0c3: f32, r1c0: f32, r1c1: f32, r1c2: f32, r1c3: f32) -> Mat2x4 {
-  Mat2x4 { r0c0, r0c1, r0c2, r1c0, r1c1, r1c2, r0c3, r1c3 }
+pub const fn mat2x4_rows(
+  r0c0: f32, r1c0: f32, r0c1: f32, r1c1: f32, r0c2: f32, r1c2: f32, r0c3: f32, r1c3: f32,
+) -> Mat2x4 {
+  Mat2x4 { r0c0, r1c0, r0c1, r1c1, r0c2, r1c2, r0c3, r1c3 }
+}
+
+pub const fn mat3x2_rows(
+  r0c0: f32, r1c0: f32, r2c0: f32, r0c1: f32, r1c1: f32, r2c1: f32,
+) -> Mat3x2 {
+  Mat3x2 { r0c0, r1c0, r2c0, r0c1, r1c1, r2c1 }
+}
+
+pub const fn mat3x3_rows(
+  r0c0: f32, r1c0: f32, r2c0: f32, r0c1: f32, r1c1: f32, r2c1: f32, r0c2: f32, r1c2: f32, r2c2: f32,
+) -> Mat3x3 {
+  Mat3x3 { r0c0, r1c0, r2c0, r0c1, r1c1, r2c1, r0c2, r1c2, r2c2 }
+}
+
+pub const fn mat3x4_rows(
+  r0c0: f32, r1c0: f32, r2c0: f32, r0c1: f32, r1c1: f32, r2c1: f32, r0c2: f32, r1c2: f32,
+  r2c2: f32, r0c3: f32, r1c3: f32, r2c3: f32,
+) -> Mat3x4 {
+  Mat3x4 { r0c0, r1c0, r2c0, r0c1, r1c1, r2c1, r0c2, r1c2, r2c2, r0c3, r1c3, r2c3 }
+}
+
+pub const fn mat4x2_rows(
+  r0c0: f32, r1c0: f32, r2c0: f32, r3c0: f32, r0c1: f32, r1c1: f32, r2c1: f32, r3c1: f32,
+) -> Mat4x2 {
+  Mat4x2 { r0c0, r1c0, r2c0, r3c0, r0c1, r1c1, r2c1, r3c1 }
+}
+
+pub const fn mat4x3_rows(
+  r0c0: f32, r1c0: f32, r2c0: f32, r3c0: f32, r0c1: f32, r1c1: f32, r2c1: f32, r3c1: f32,
+  r0c2: f32, r1c2: f32, r2c2: f32, r3c2: f32,
+) -> Mat4x3 {
+  Mat4x3 { r0c0, r1c0, r2c0, r3c0, r0c1, r1c1, r2c1, r3c1, r0c2, r1c2, r2c2, r3c2 }
+}
+
+pub const fn mat4x4_rows(
+  r0c0: f32, r1c0: f32, r2c0: f32, r3c0: f32, r0c1: f32, r1c1: f32, r2c1: f32, r3c1: f32,
+  r0c2: f32, r1c2: f32, r2c2: f32, r3c2: f32, r0c3: f32, r1c3: f32, r2c3: f32, r3c3: f32,
+) -> Mat4x4 {
+  Mat4x4 {
+    r0c0,
+    r1c0,
+    r2c0,
+    r3c0,
+    r0c1,
+    r1c1,
+    r2c1,
+    r3c1,
+    r0c2,
+    r1c2,
+    r2c2,
+    r3c2,
+    r0c3,
+    r1c3,
+    r2c3,
+    r3c3,
+  }
 }
