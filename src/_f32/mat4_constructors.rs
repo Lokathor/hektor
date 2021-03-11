69cb9261bbd26627dc0974bbf46125f261fb9368
Lokathor <zefria@gmail.com>
2021-03-10 23:54:12 -0700

    rename

--- src/_f32/mat4_constructors.rs
+++ src/_f32/mat4_constructors.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 impl Mat4x4 {
-  pub fn translation(Vec3 { x, y, z }: Vec3) -> Self {
+  pub fn from_translation(Vec3 { x, y, z }: Vec3) -> Self {
     mat4x4_rows(
       [1.0, 0.0, 0.0, x],
       [0.0, 1.0, 0.0, y],
@@ -10,7 +10,7 @@ impl Mat4x4 {
     )
   }
 
-  pub fn scale(Vec3 { x, y, z }: Vec3) -> Self {
+  pub fn from_scale(Vec3 { x, y, z }: Vec3) -> Self {
     mat4x4_rows(
       [x, 0.0, 0.0, 0.0],
       [0.0, y, 0.0, 0.0],
@@ -20,7 +20,7 @@ impl Mat4x4 {
   }
 
   /// radians!
-  pub fn x_rotation(r: f32) -> Self {
+  pub fn from_x_rotation(r: f32) -> Self {
     mat4x4_rows(
       [1.0, 0.0, 0.0, 0.0],
       [0.0, r.cos(), r.sin(), 0.0],
@@ -30,7 +30,7 @@ impl Mat4x4 {
   }
 
   /// radians!
-  pub fn y_rotation(r: f32) -> Self {
+  pub fn from_y_rotation(r: f32) -> Self {
     mat4x4_rows(
       [r.cos(), 0.0, -(r.sin()), 0.0],
       [0.0, 1.0, 0.0, 0.0],
@@ -40,7 +40,7 @@ impl Mat4x4 {
   }
 
   /// radians!
-  pub fn z_rotation(r: f32) -> Self {
+  pub fn from_z_rotation(r: f32) -> Self {
     mat4x4_rows(
       [r.cos(), -r.sin(), 0.0, 0.0],
       [r.sin(), r.cos(), 0.0, 0.0],
@@ -51,7 +51,7 @@ impl Mat4x4 {
 
   /// radians!
   #[rustfmt::skip]
-  pub fn xyz_rotation(x: f32, y: f32, z: f32) -> Self {
+  pub fn from_xyz_rotation(x: f32, y: f32, z: f32) -> Self {
     let cx = x.cos();
     let cy = y.cos();
     let cz = z.cos();
@@ -84,7 +84,7 @@ impl Mat4x4 {
   }
 
   #[rustfmt::skip]
-  pub fn look_at(eye_pos: Vec3, target: Vec3, up: Vec3) -> Self {
+  pub fn from_look_at(eye_pos: Vec3, target: Vec3, up: Vec3) -> Self {
     let front = (target - eye_pos).normalized();
     let right = front.cross(up).normalized();
     let up = right.cross(front);
@@ -97,7 +97,7 @@ impl Mat4x4 {
   }
 
   #[rustfmt::skip]
-  pub fn perspective(v_fov: f32, aspect_ratio: f32, z_near: f32, z_far: f32) -> Self {
+  pub fn from_perspective(v_fov: f32, aspect_ratio: f32, z_near: f32, z_far: f32) -> Self {
     let t = (v_fov * 0.5).tan();
     let sy = 1.0 / t;
     let sx = sy / aspect_ratio;
@@ -111,7 +111,7 @@ impl Mat4x4 {
   }
 
   #[rustfmt::skip]
-  pub fn orthographic(left: f32, right: f32, bottom: f32, top: f32, near: f32, far: f32) -> Self {
+  pub fn from_orthographic(left: f32, right: f32, bottom: f32, top: f32, near: f32, far: f32) -> Self {
     let r_m_l = right - left;
     let r_p_l = right + left;
     let t_m_b = top - bottom;
